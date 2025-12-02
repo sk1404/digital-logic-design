c82402eb1d625ee866c22ca88f5484f83d9ca8ad
sk1404 <sheshadri1404@gmail.com>
2025-12-02 12:04:53 +0530

    Create step.v

---
+++ step.v
@@ -0,0 +1,13 @@
+ ssh -X pgstudent12@10.40.40.45
+enter password 
+csh 
+source cshrc_new
+cd counter_design_database_45nm/
+cd simulation
+create folder 
+create files: name gedit 4fa.v 4fa_tb.v
+xrun -access +rwc 4fa.v 4fa_tb.v -gui -coverage all
+
+if no error new window will open waveform and view 
+
+then click coverage all if coverage all above 95% then its good else re written code 
